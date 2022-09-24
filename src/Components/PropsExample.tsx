d7f430f41f88020cb566607e4b63202ec215527d
Devendra Kumar <devendra.kumar@devendra224ub.successivetech.com>
2022-09-24 11:15:22 +0530

    FC example and props destructuring

--- src/Components/PropsExample.tsx
+++ src/Components/PropsExample.tsx
@@ -6,7 +6,17 @@ interface Props {
     email?: string
 }
 
-// Example one of destructuring
+//-------------------Optional props value ---------------
+// ------------------------------------------------------
+
+// interface Props {
+//     name?: string,
+//     age?: number,
+//     email?: string
+// }
+
+//------------------- Example one ------------------------
+// -------------------------------------------------------
 
 // const PropsExample = (props : Props) => {
 //     const { name, age, email } = props;
@@ -17,15 +27,40 @@ interface Props {
 //    )
 // }
 
-// Example Two of destructuring
+//------------------- Example two With destructuring ------------
+// --------------------------------------------------------------
 
-const PropsExample = ({ name, age, email }  : Props) => {
+// const PropsExample = ({ name, age, email }  : Props) => {
   
-   return (
-    <div>
-      {name }, {age} , {email}
-    </div>
-   )
-}
+//    return (
+//     <div>
+//       {name }, {age} , {email}
+//     </div>
+//    )
+// }
+
+//------------------- Example two With FC ( functional component ) ------
+//------------------- With destructuring --------------------------------
+
+// const PropsExample:FC<Props> = ({ name, age, email }) => {
+
+//     return (
+//      <div>
+//        {name }, {age} , {email}
+//      </div>
+//     )
+//  }
+
+//------------------- Example Four ----------------------
+// ------------------------------------------------------
+
+const PropsExample:FC<Props> = (props) => {
+    const { name, age, email } = props;
+     return (
+      <div>
+        {name }, {age} , {email}
+      </div>
+     )
+  }
 
 export default PropsExample;
