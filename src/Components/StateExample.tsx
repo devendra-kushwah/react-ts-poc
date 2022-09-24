dcd7d4c726645b9e5fb7d44afee306c71e9ef54c
Devendra kushwaha <dev297146@gmail.com>
2022-09-24 20:19:44 +0530

    added state examples (#6)
    Co-authored-by: Devendra Kumar <devendra.kumar@devendra224ub.successivetech.com>

---
+++ src/Components/StateExample.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+function StateExample() {
+      
+    const [name, setName] = useState<string>("") // string example  
+    const [age, setAge] = useState<number | null>() //  example  with null value
+  
+  return (
+    <div>
+     Hi There I am TS 
+     
+    </div>
+  );
+}
+
+export default StateExample;
