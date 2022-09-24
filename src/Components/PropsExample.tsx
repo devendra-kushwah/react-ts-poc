3ec43c0fcc9fb9c70c1af7e9adc1314447ccf560
Devendra Kumar <devendra.kumar@devendra224ub.successivetech.com>
2022-09-24 10:37:27 +0530

    first commit

---
+++ src/Components/PropsExample.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from "react";
+
+interface Props {
+    name?: string,
+    age?: number,
+    email?: string
+}
+
+// optional props value
+
+// interface Props {
+//     name?: string,
+//     age?: number,
+//     email?: string
+// }
+
+// Example one 
+// const PropsExample = (props : Props) => {
+//     const { name, age, email } = props;
+//    return (
+//     <div>
+//       {name }, {age} , {email}
+//     </div>
+//    )
+// }
+
+// Example two With destructuring 
+
+// const PropsExample = ({ name, age, email }  : Props) => {
+  
+//    return (
+//     <div>
+//       {name }, {age} , {email}
+//     </div>
+//    )
+// }
+
+// Example two With FC ( functional component )
+
+const PropsExample:FC<Props> = ({ name, age, email }) => {
+
+    return (
+     <div>
+       {name }, {age} , {email}
+     </div>
+    )
+ }
+
+export default PropsExample;
