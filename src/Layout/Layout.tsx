596c0bef9b81dbe93fb2f0dc4d5780d76c61f7d1
Devendra Kumar <devendra@devendrass586.local>
2022-10-30 22:26:18 +0530

    added class prop in layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -2,15 +2,16 @@ import React, { ReactNode } from "react";
 import Container from '@mui/material/Container';
 import Header from "./Header/Header";
 
-type  Props = {
-   children?: ReactNode
+type Props = {
+   children?: ReactNode,
+   className?: string
 }
 
-const Layout = ({ children }: Props)=> {
+const Layout = ({ children, className }: Props)=> {
      return (
       <>
       <Header />
-      <main>
+      <main className={className || "main-content"}>
         <Container>
           {children}
         </Container>
