6fbd953e448f4445586d976b022bb5ada25bfff9
dev-successive <devendra@devendrass586.local>
2022-12-31 23:43:45 +0530

    fixed conflicts

---
+++ src/modules/Private/Dashboard/index 2.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+import Layout from "../../../Layout/Layout";
+
+const Dashboard = () => {
+
+  return (
+    <Layout>
+      <h1>Welcom to the Dashboard</h1>
+  </Layout>
+  )
+}
+
+export default Dashboard;
