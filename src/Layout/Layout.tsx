b26e70664bb80ddf4aaa5a1cfb1d3e5867738789
devendra-kushwah <dkumar1@k12.com>
2023-08-15 22:39:27 +0530

    added sidebar menu

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import Box from "@mui/material/Box";
 import Header from "layout/header/Header";
+import SidebarMenu from "./sidebarMenu";
 
 type Props = {
   children?: ReactNode;
@@ -19,7 +20,7 @@ const Layout = (props: Props) => {
   return (
     <>
       {enableHeader && <Header />}
-
+      {/* <SidebarMenu /> */}
       <Box component="main" sx={sx} className={className || "main-content"}>
         {children}
       </Box>
