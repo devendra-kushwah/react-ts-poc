05e068f4a2e48f4688512d40cbc64e356044b3b1
devendra-kushwah <dkumar1@k12.com>
2023-08-19 17:07:53 +0530

    fixed style issues

--- src/Layout/Dashboard/DrawerMenu.tsx
+++ src/Layout/Dashboard/DrawerMenu.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import MuiDrawer from "@mui/material/Drawer";
-import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+
 import {
   List,
   ListItem,
@@ -12,48 +11,7 @@ import {
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 import { primary } from "./menuConfig";
-
-const drawerWidth = 240;
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-  ".MuiDrawer-paper": {
-    marginTop: 63,
-  },
-}));
+import { Drawer } from "./style";
 
 interface Props {
   open?: boolean;
